af47528a4bf85682c0c220db9766c33edc672ae4
Gilles Felix <felix@novius.fr>
2025-05-28 18:01:00 +0200

    Phpstan

--- src/Filament/Forms/Components/ExpiredAt.php
+++ src/Filament/Forms/Components/ExpiredAt.php
@@ -5,10 +5,8 @@ namespace Novius\LaravelFilamentPublishable\Filament\Forms\Components;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Get;
-use Illuminate\Database\Eloquent\Model;
 use Novius\LaravelFilamentPublishable\Filament\Traits\IsPublishable;
 use Novius\LaravelPublishable\Enums\PublicationStatus;
-use Novius\LaravelPublishable\Traits\Publishable;
 
 class ExpiredAt extends DateTimePicker
 {
@@ -27,7 +25,6 @@ class ExpiredAt extends DateTimePicker
         $this->after(function (Get $get, Component $component) {
             $modelClass = $component->getModel();
 
-            /** @var Model&Publishable $record */
             return $this->isPublishable($modelClass) ? $get($this->publishableModel($modelClass)->getPublishedAtColumn()) : null;
         });
     }
